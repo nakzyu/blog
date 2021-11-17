91ec6ded524db441c804a983bc6fb9717ebe490f
nakzyu <nakzyu@gmail.com>
2021-11-17 22:28:01 +0900

    Feat: styled components structures

---
+++ src/styles/components/title.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: rgb(41, 41, 41);
+  font-size: 46px;
+  font-weight: bold;
+
+  @media screen and (max-width: 727.98px) {
+    font-size: 32px;
+  }
+`;
+
+export default Title;
