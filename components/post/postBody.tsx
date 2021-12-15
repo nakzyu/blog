7c16145100f78f659694df78fb3ad122b95b6429
nakzyu <nakzyu@gmail.com>
2021-12-16 00:25:59 +0900

    NWC

--- components/post/postBody.tsx
+++ components/post/postBody.tsx
@@ -1,8 +1,9 @@
 import { Post } from "../../types/post";
 import ReactMarkdown from "react-markdown";
+import "./postBody.module.css";
 
 const PostBody = ({ content }: Post) => {
-  return <ReactMarkdown className='markdown-body'>{content}</ReactMarkdown>;
+  return <ReactMarkdown className='reactMarkDown'>{content}</ReactMarkdown>;
 };
 
 export default PostBody;
--- components/post/postCard.tsx
+++ components/post/postCard.tsx
@@ -9,7 +9,7 @@ const PostCard = ({ data }: PostCardProps) => {
   return (
     <>
       <img className='cursor-pointer' src='/images/profile.jpg' alt='image' />
-      <h3 className='cursor-pointer'>{data.title}</h3>
+      <h2 className='cursor-pointer'>{data.title}</h2>
       <p className='cursor-pointer'>{data.description}</p>
       <div className='cursor-pointer'>{data.publishedDate}</div>
     </>
