3a6038900e583b3f5ec53a204839bc647f08ffb9
nakzyu <nakzyu@gmail.com>
2022-03-12 08:13:08 +0900

    Feature: Display accurate published date-time on hover

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className='mx-4 lg:mx-0 h-full flex justify-end items-center'>
-      <p className='mr-5'>nakzyu@gmail.com</p>
+      <p className='mr-3'>nakzyu@gmail.com</p>
       <div className='flex'>
-        <Link href='https://github.com/nakzyu/blog' passHref>
+        <Link href='https://github.com/nakzyu' passHref>
           <img
-            className=' cursor-pointer'
-            src='images/GitHub-Mark-Light-32px.png'
+            className=' cursor-pointer w-6 h-6'
+            src='images/GitHub-Mark-Light-64px.png'
             alt='githubMark'
           />
         </Link>
--- components/post/timeToolTip.tsx
+++ components/post/timeToolTip.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { fromNow } from "../../utils/dateHandler";
 
 type TimeToolTipProps = {
@@ -5,9 +6,33 @@ type TimeToolTipProps = {
 };
 
 const TimeToolTip = ({ publishedDate }: TimeToolTipProps) => {
+  const [isToolTipVisible, setIsToolTipVisible] = useState(false);
+
+  const mouseEvt = {
+    onMouseEnter: () => setIsToolTipVisible(true),
+    onMouseLeave: () => {
+      setIsToolTipVisible(false);
+    },
+  };
+
   return (
-    <div className='mt-0.5'>
-      <p className='cursor-pointer'>{fromNow(publishedDate)}</p>
+    <div
+      className=' pt-0.5 pb-1 pr-2  flex items-center content-center'
+      {...mouseEvt}
+    >
+      <p className='cursor-pointer pr-1'>{fromNow(publishedDate)}</p>
+      <img
+        className='w-4 h-4 mt-0.5 cursor-pointer'
+        src='images/info.svg'
+        alt='info-icon'
+      />
+      <div
+        className={`${
+          isToolTipVisible ? "visible" : "hidden"
+        } flex relative items-center justify-center pl-1`}
+      >
+        <p>{publishedDate.split(":").slice(0, -1).join(":")}</p>
+      </div>
     </div>
   );
 };
