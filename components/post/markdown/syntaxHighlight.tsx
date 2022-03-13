0eede7bad2ab5fe8c9dbf10114db1963a05312b0
nakzyu <nakzyu@gmail.com>
2022-03-13 13:30:39 +0900

    Fix: Empty line of makrdown-code

--- components/post/markdown/syntaxHighlight.tsx
+++ components/post/markdown/syntaxHighlight.tsx
@@ -1,3 +1,4 @@
+import { ReactChild } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark as theme } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
@@ -5,11 +6,14 @@ const SyntaxHighlight: object = {
   code({
     inline,
     className,
+    children,
     ...props
   }: {
     inline: boolean;
     className: string;
+    children: string[];
   }) {
+    console.log(children);
     const match = /language-(\w+)/.exec(className || "");
     return !inline && match ? (
       <SyntaxHighlighter
@@ -19,11 +23,13 @@ const SyntaxHighlight: object = {
         className='codeStyle'
         showLineNumbers={true}
         {...props}
-      />
+      >
+        {children[0].slice(0, children[0].length - 1)}
+      </SyntaxHighlighter>
     ) : (
       <code className={className} {...props} />
     );
   },
 };
 
-export default SyntaxHighlight as object;
+export default SyntaxHighlight;
