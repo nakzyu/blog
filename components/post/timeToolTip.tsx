0577cb51084e322a8ba314502c9199676e04120f
nakzyu <nakzyu@gmail.com>
2022-03-14 21:10:01 +0900

    Feature: Change body layout, Delete hover-event of datetime

--- components/post/timeToolTip.tsx
+++ components/post/timeToolTip.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { fromNow } from "../../utils/dateHandler";
 
 type TimeToolTipProps = {
@@ -6,31 +5,11 @@ type TimeToolTipProps = {
 };
 
 const TimeToolTip = ({ publishedDate }: TimeToolTipProps) => {
-  const [isToolTipVisible, setIsToolTipVisible] = useState(false);
-
-  const mouseEvt = {
-    onMouseEnter: () => setIsToolTipVisible(true),
-    onMouseLeave: () => {
-      setIsToolTipVisible(false);
-    },
-  };
-
   return (
-    <div
-      className=' pt-0.5 pb-1 pr-2  flex items-center content-center'
-      {...mouseEvt}
-    >
-      <p className='cursor-pointer pr-1'>{fromNow(publishedDate)}</p>
-      <img
-        className='w-4 h-4 mt-0.5 cursor-pointer'
-        src='/images/info.svg'
-        alt='info-icon'
-      />
-      <div
-        className={`${
-          isToolTipVisible ? "visible" : "hidden"
-        } flex relative items-center justify-center pl-1`}
-      >
+    <div className=' pt-0.5 pb-1 pr-2  flex items-center content-center'>
+      <p className=' pr-1'>{fromNow(publishedDate)}</p>
+      <img className='w-4 h-4 mt-0.5 ' src='/images/info.svg' alt='info-icon' />
+      <div className='flex relative items-center justify-center pl-1'>
         <p>{publishedDate.split(":").slice(0, -1).join(":")}</p>
       </div>
     </div>
